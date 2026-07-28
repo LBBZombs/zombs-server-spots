6950db18fd2c30e984ad39ad31b70b3867712064
LBBZombs <139074757+LBBZombs@users.noreply.github.com>
2026-07-28 21:20:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -49085,7 +49085,7 @@ var Game =
                 if (!data.allowed) {
                     return;
                 }
-                game.ui.components.PopupOverlay.showHint("Last updated " + new Date(1784612400000))
+                game.ui.components.PopupOverlay.showHint("Last updated " + new Date(1785266400000))
                 var tickRate = data.tickRate;
                 this.msPerTick = 1000 / tickRate;
                 this.msInThisTick = 0;
